65999921afb39e6bb3f7b11502e6c1163bd49b63
sahlag <boussaid-souheil@live.fr>
2018-06-04 14:19:52 +0200

    envoi de message

--- public/javascripts/tchat-client.js
+++ public/javascripts/tchat-client.js
@@ -3,13 +3,27 @@ console.log('ouverture de tchat coté client');
 let socket = io();
 
 let formConnection = document.getElementById('form-connection');
+let formMessage = document.getElementById('form-message');
 let inputName = document.getElementById('name');
+let inputMessage = document.getElementById('name');
 
+// Gestion de l'authentification.
 formConnection.addEventListener('submit', (event)=>{
   event.preventDefault();
   console.log('Authentification');
   const username = inputName.value;
 
-  // Emession de l'événement vers le serveur
+// Emession de l'événement vers le serveur
   socket.emit('newUser', username);
 })
+
+// Gestion de message.
+formMessage.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    console.log("Envoi d'un message");
+    const message = inputMessage.value;
+  
+ // Emession de l'événement vers le serveur
+    socket.emit('newMessage', message);
+  })
+
--- util/socket.js
+++ util/socket.js
@@ -5,10 +5,19 @@ module.exports.tchat = function tchat(io) {
     io.on('connection', (socket) =>{
         console.log('Nouvel utilisateur connecté !');
 
+    let identity = null;
+    
     // Traitement de l'évenement newUser
     socket.on('newUser', (username)=>{
         console.log('Authentification (coté serveur)');
         console.log(username);
-    })
+        identity = username;
+    });
+
+   // Traitement de l'évenement newUser
+   socket.on('newMessage', (message)=>{
+    console.log(`${identity} a envoyer un message`);
+    // on difuse le message  a tous les clients connectés
+   })
 });
 }
